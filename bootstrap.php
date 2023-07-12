0960e34b25b978195872ebd027013b0637001986
unknown <nguyenviet212002@gmail.com>
2023-07-12 12:22:52 +0700

    change name project

--- bootstrap.php
+++ bootstrap.php
@@ -1,16 +1,17 @@
 <?php
-define('_DIR_ROOT_',__DIR__); // gán biến SERVER vào _DIR_ROOT_
+define('_DIR_ROOT_', __DIR__); // gán biến SERVER vào _DIR_ROOT_
 
 if (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') {
-    $web_root = 'https://'.$_SERVER['HTTP_HOST'];
-}else {
-    $web_root = 'http://'.$_SERVER['HTTP_HOST'];
+    $web_root = 'https://' . $_SERVER['HTTP_HOST'];
+} else {
+    $web_root = 'http://' . $_SERVER['HTTP_HOST'];
 }
 
-$folder = strstr(strtolower(_DIR_ROOT_),'web-ban-hang-mvc');//nối tên SERVER và web-ban-hang-mvc
-$web_root = $web_root.'/'.$folder; //thêm dấu '/' 
 
-define('_FOLDER_ROOT_',$web_root); //gán biến SERVER và tên dự án http://localhost/web-ban-hang-mvc cho _FOLDER_ROOT_
+$folder = strstr(strtolower(_DIR_ROOT_), 'web-shop-mobile'); //nối tên SERVER và web-ban-hang-mvc
+$web_root = $web_root . '/' . $folder; //thêm dấu '/' 
+
+define('_FOLDER_ROOT_', $web_root); //gán biến SERVER và tên dự án http://localhost/web-ban-hang-mvc cho _FOLDER_ROOT_
 
 // require tất cả các thư mục cần sử dụng
 require_once 'config/routes.php';
